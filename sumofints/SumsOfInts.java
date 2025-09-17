6fbb7c3d6015c8642cd9c767569ab79247f1a7ea
Abel Saberhagen <abelsaberhagen@gmail.com>
2025-09-17 10:41:40 -0400

    Sum of ints

--- sumofints/SumsOfInts.java
+++ sumofints/SumsOfInts.java
@@ -1,6 +1,8 @@
-public class SumsAlgorithm {
+package sumofints;
+
+
+public class SumsOfInts {
     public static void main(String args[]) {
-        System.out.println("Hello");
         int[] nums = { 2, 3, 6, 8, 5, 9, 14 };
         int sum = 17;
         for (int i = 0; i < nums.length; i++) {
