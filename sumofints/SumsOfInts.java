30d4440de2e38895b2575aba05dc41537bf65f04
Abel Saberhagen <abelsaberhagen@gmail.com>
2025-09-22 14:36:54 -0400

    Edit 1

--- sumofints/SumsOfInts.java
+++ sumofints/SumsOfInts.java
@@ -6,10 +6,9 @@ public class SumsOfInts {
         int[] nums = { 2, 3, 6, 8, 5, 9, 14 };
         int sum = 17;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] + nums[j] == sum) {
-                    System.out.println(Integer.toString(nums[i]) + " + " + Integer.toString(nums[j]) + " = " + sum);
-
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == sum && nums[i] != nums[j]) {
+                    System.out.println(nums[i] + " + " + nums[j] + " = " + sum);
                 }
             }
         }
